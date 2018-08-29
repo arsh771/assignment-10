28f6cb0d3f492997282e4672b00811b6bea5eb47
arsh771 <arshk332211@gmail.com>
2018-08-29 19:38:28 +0530

    assignment-10

---
+++ File Handling.py
@@ -0,0 +1,60 @@
+#Q1
+
+f=open('file1.txt','r')
+new=f.read()
+print(new)
+f.close()
+print()
+
+
+#Q2
+
+f=open("file1.txt",'r')
+data=f.read()
+word=data.split()
+dict={}
+for x in word:
+    dict[x]=word.count(x)
+print(dict)
+print()
+
+
+#Q3
+
+with open('file1.txt', 'r') as f1:
+    with open('file2.txt', 'w') as f2:
+        f2.write(f1.read())
+
+
+#Q4
+str1=[]
+str2=[]
+s=str()
+with open('file1.txt','r') as f1:
+    with open('file2.txt','r') as f2:
+       str1+=f1.readlines()
+       str2+=f2.readlines()
+       for x,y in zip(str1,str2):
+           print(x)
+           print(y)
+           s+=x+y
+with open('file3.txt','w') as f3:
+      f3.write(s)
+
+
+
+#Q5
+with open('file4.txt','w') as f:
+   for i in range(10):
+      x=int(input("Enter number: "))
+      f.write("%d "%(x))
+
+with open('file4.txt','r') as f:
+   data=f.readlines()
+   for no in data:
+       a=no.split()
+   a.sort()
+
+with open('file5.txt','w') as f:
+   for i in a:         
+      f.write("%s "%(i))
